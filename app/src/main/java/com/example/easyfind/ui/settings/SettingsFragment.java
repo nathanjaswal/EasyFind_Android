d6f2d67a8be3ae1d5bda0f4803220f17a93a0582
Ramanpreet Singh <ramanpreet230@gmail.com>
2020-02-14 14:23:20 -0500

    Code & design updating.

---
+++ app/src/main/java/com/example/easyfind/ui/settings/SettingsFragment.java
@@ -0,0 +1,25 @@
+package com.example.easyfind.ui.settings;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.easyfind.R;
+
+public class SettingsFragment extends Fragment {
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_settings, container, false);
+        return root;
+    }
+}
